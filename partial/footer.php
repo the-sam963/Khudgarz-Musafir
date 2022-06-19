19b0c4fe37144c4d361a7e61e25023b5caf559db
Samrat <samratbiswas986@gmail.com>
2022-06-20 01:02:24 +0530

    Initial commit of all files to the repository

---
+++ partial/footer.php
@@ -0,0 +1,33 @@
+ <!-- SOCIAL MEDIA SECTION  -->
+ <section class="social ">
+    <div class="social-top">
+        <div class="icon-container width-2 mid">
+            <div class="icon-box">
+                <a href="https://www.facebook.com/khudgarzmusafir/"><i class="fab fa-facebook"> Facebook</i></a>
+            </div>
+            <div class="icon-box">
+                <a href="https://www.youtube.com/c/khudgarzmusafir/"><i class="fab fa-youtube"> YouTube</i></a>
+            </div>
+            <div class="icon-box">
+                <a href="https://www.instagram.com/khudgarzmusafir/"><i class="fab fa-instagram"> Instagram</i></a>
+            </div>
+            <div class="icon-box">
+                <a href="https://www.twitter.com/khudgarzmusafir/"><i class="fab fa-twitter"> Twitter</i></a>
+            </div>
+        </div>
+    </div>
+    <div class="social-bottom width-1 mid">
+        <script src="https://apps.elfsight.com/p/platform.js" defer></script>
+        <div class="elfsight-app-f7ad88fe-4525-4797-a73a-a3f7a2d745b9"></div>
+    </div>
+</section>
+
+<!-- FOOTER SECTION  -->
+<footer>
+    <p>All Rights Resurved | www.khudgarzmusafir.com</p>
+    <p>Copyright&copy; 2020 - </p>
+
+</footer>
+</body>
+<script src="../JS/scrol-ind.js"></script>
+<script src="../JS/typing.js"></script>
---
+++ partial/nav.php
@@ -0,0 +1,19 @@
+<nav class="navigation font1 bg-main">
+    <div class="nav-left">
+        <img class="logo" src="../IMG/logo.png">
+        <ul class="navbar">
+            <li><a href="../HTML/index.html">Home</a></li>
+            <li><a href="../HTML/bogpost.html">BlogPost</a></li>
+            <li><a href="../HTML/explore.html">Explore</a></li>
+            <li><a href="https://www.youtube.com/channel/UCZocERCeXzn8Bfw_f4ssqcw" target="_blank">YouTube</a></li>
+            <li><a href="../HTML/contact.html">Contact</a></li>
+        </ul>
+    </div>
+    <form class="nav-right" action="../HTML/search.html" method="GET">
+        <input action="../HTML/search.html" type="search" name="search" id="search" class="search"
+            placeholder="Search...">
+    </form>
+</nav>
+
+<!-- scrol-ind -->
+<div id="scrl-bar"></div>
